566a662bd02c606ca7c007f69fcbc15f53d2fa37
j516143@gmail.com <j516143@gmail.com>
2020-04-22 16:15:30 +0800

    add

---
+++ logger.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+import time
+
+
+Fs = 100.0;  # sampling rate
+Ts = 10.0/Fs; # sampling interval
+t = np.arange(0,10,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
+x = np.arange(0,10,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
+y = np.arange(0,10,Ts) # signal vector; create Fs samples
+z = np.arange(0,10,Ts) # signal vector; create Fs samples
+tilt = np.arange(0,10,Ts)
+
+serdev = '/dev/ttyACM0'
+s = serial.Serial(serdev,115200)
+for i in range(300):
+    line=s.readline()
+    #print (line)
+    if(i%3 == 0): x[int(i/3)] = float(line)
+    elif(i%3 == 1): y[int(i/3)] = float(line)
+    elif(i%3 == 2): z[int(i/3)] = float(line)
+   
+
+for i in range(100):
+    if z[i]<0.5 or abs(x[i])>0.5 or abs(y[i])>0.5:
+        tilt[i]=1
+    else:
+        tilt[i]=0
+
+fig, ax = plt.subplots(2, 1)
+
+ax[0].plot(t,x, label = 'x')
+ax[0].plot(t,y, label = 'y')
+ax[0].plot(t,z, label = 'z')
+ax[0].set_xlabel('t')
+ax[0].set_ylabel('vector')
+ax[0].legend()
+
+ax[1].stem(t,tilt) 
+ax[1].set_xlabel('time')
+ax[1].set_ylabel('tilt')
+
+plt.show()
+
+s.close()
---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include "mbed.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+#define UINT14_MAX        16383
+// FXOS8700CQ I2C address
+#define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR1 (0x1D<<1) // with pins SA0=1, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR2 (0x1C<<1) // with pins SA0=0, SA1=1
+#define FXOS8700CQ_SLAVE_ADDR3 (0x1F<<1) // with pins SA0=1, SA1=1
+// FXOS8700CQ internal register addresses
+#define FXOS8700Q_STATUS 0x00
+#define FXOS8700Q_OUT_X_MSB 0x01
+#define FXOS8700Q_OUT_Y_MSB 0x03
+#define FXOS8700Q_OUT_Z_MSB 0x05
+#define FXOS8700Q_M_OUT_X_MSB 0x33
+#define FXOS8700Q_M_OUT_Y_MSB 0x35
+#define FXOS8700Q_M_OUT_Z_MSB 0x37
+#define FXOS8700Q_WHOAMI 0x0D
+#define FXOS8700Q_XYZ_DATA_CFG 0x0E
+#define FXOS8700Q_CTRL_REG1 0x2A
+#define FXOS8700Q_M_CTRL_REG1 0x5B
+#define FXOS8700Q_M_CTRL_REG2 0x5C
+#define FXOS8700Q_WHOAMI_VAL 0xC7
+I2C i2c( PTD9,PTD8);
+Serial pc(USBTX, USBRX);
+DigitalOut redled(LED1);
+InterruptIn btn(SW3); 
+EventQueue queue1;
+Thread thread1;
+int m_addr = FXOS8700CQ_SLAVE_ADDR1;
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
+   char t = addr;
+   i2c.write(m_addr, &t, 1, true);
+   i2c.read(m_addr, (char *)data, len);
+}
+
+void FXOS8700CQ_writeRegs(uint8_t * data, int len) {
+   i2c.write(m_addr, (char *)data, len);
+}
+float t[3];
+void record(){
+    pc.printf("%1.4f\r\n",t[0]);
+    pc.printf("%1.4f\r\n",t[1]);
+    pc.printf("%1.4f\r\n",t[2]);
+   redled = !redled;
+   wait(0.1);
+}
+
+void btn_p()  {
+    int i;
+    for (i=0; i<100; i++){
+        queue1.call(record);
+    }   
+}
+
+int main() {
+    pc.baud(115200);
+    uint8_t who_am_i, data[2], res[6];
+    int16_t acc16;
+   
+    int i;
+   // Enable the FXOS8700Q
+
+   FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
+   data[1] |= 0x01;
+   data[0] = FXOS8700Q_CTRL_REG1;
+   FXOS8700CQ_writeRegs(data, 2);
+
+   // Get the slave address
+   FXOS8700CQ_readRegs(FXOS8700Q_WHOAMI, &who_am_i, 1);
+
+   //pc.printf("Here is %x\r\n", who_am_i);
+    
+    redled = 1;
+    thread1.start(callback(&queue1, &EventQueue::dispatch_forever));
+    btn.rise(queue1.event(btn_p));   
+    while(1){
+      FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+
+      acc16 = (res[0] << 6) | (res[1] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[0] = ((float)acc16) / 4096.0f;
+
+      acc16 = (res[2] << 6) | (res[3] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[1] = ((float)acc16) / 4096.0f;
+
+      acc16 = (res[4] << 6) | (res[5] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      t[2] = ((float)acc16) / 4096.0f;
+    }
+    
+}
